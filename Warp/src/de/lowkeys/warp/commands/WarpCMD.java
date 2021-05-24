9974ec4254866225d4440bb308f88d85663537fd
Gero Lindner <gero.lindner@outlook.de>
2021-05-24 19:37:04 +0200

    Added /warp command (no function yet)

---
+++ Warp/src/de/lowkeys/warp/commands/WarpCMD.java
@@ -0,0 +1,30 @@
+package de.lowkeys.warp.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.lowkeys.core.api.Common;
+
+public class WarpCMD implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] args) {
+		
+		if(!(sender instanceof Player)) {
+			return false;
+		}
+		
+		Player player = (Player) sender;
+		
+		if(!player.hasPermission("lowkeys.warp.warp")) {
+			player.sendMessage(Common.getNoPerms());
+			
+			return false;
+		}
+		
+		return false;
+	}
+
+}
--- Warp/src/de/lowkeys/warp/main/Warp.java
+++ Warp/src/de/lowkeys/warp/main/Warp.java
@@ -3,10 +3,13 @@ package de.lowkeys.warp.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.lowkeys.core.api.Common;
+import de.lowkeys.warp.commands.WarpCMD;
 
 public class Warp extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		this.getCommand("warp").setExecutor(new WarpCMD());
+		
 		Common.log("§7Warp loaded!");
 	}
 	
