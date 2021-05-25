c442585a4a03beafe5c0620cbeba838a32c00d8a
Gero Lindner <gero.lindner@outlook.de>
2021-05-25 08:50:37 +0200

    Added /editwarp command (no function yet)

---
+++ Warp/src/de/lowkeys/warp/commands/EditWarpCMD.java
@@ -0,0 +1,25 @@
+package de.lowkeys.warp.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.lowkeys.core.api.Common;
+
+public class EditWarpCMD implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] args) {
+		if(!(sender instanceof Player)) return false;
+		
+		Player player = (Player) sender;
+		
+		if(!player.hasPermission("lowkeys.warp.editwarp")) {
+			player.sendMessage(Common.getNoPerms());
+		}
+		
+		return false;
+	}
+
+}
